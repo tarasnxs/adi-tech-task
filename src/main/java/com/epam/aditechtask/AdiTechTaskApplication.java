b4807ce4ceba5dcb5f0aa97600b17de357f9cdbf
Taras_Hrodzitskyi <Taras_Hrodzitskyi@epam.com>
2025-03-03 09:43:39 +0200

    init

---
+++ src/main/java/com/epam/aditechtask/AdiTechTaskApplication.java
@@ -0,0 +1,13 @@
+package com.epam.aditechtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiTechTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdiTechTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/aditechtask/AdiTechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.aditechtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiTechTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
